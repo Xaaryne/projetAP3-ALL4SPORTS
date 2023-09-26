664becf0bedb3c45aa6b9216974eb7da5e280cf2
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-09-26 18:33:29 +0200

    Modif sur panier ( quantite)

--- src/Entity/PANIER.php
+++ src/Entity/PANIER.php
@@ -13,8 +13,23 @@ class PANIER
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
+    private ?int $quantite = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
     }
 }
