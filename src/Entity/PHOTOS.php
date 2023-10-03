8790df9e8b76cd6027744a236b95dc97b2aedce2
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-10-03 15:00:22 +0200

    Revert "Relation Photos-Produit"
    This reverts commit d05183ea4e398f42518ab52560f182c072ac85e4.

--- src/Entity/PHOTOS.php
+++ src/Entity/PHOTOS.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\PHOTOSRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PHOTOSRepository::class)]
@@ -17,14 +15,6 @@ class PHOTOS
 
     #[ORM\Column(length: 250)]
     private ?string $photo = null;
-
-    #[ORM\OneToMany(mappedBy: 'fk_photos', targetEntity: PRODUIT::class)]
-    private Collection $fk_produit;
-
-    public function __construct()
-    {
-        $this->fk_produit = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -39,36 +29,6 @@ class PHOTOS
     public function setPhoto(string $photo): static
     {
         $this->photo = $photo;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, PRODUIT>
-     */
-    public function getFkProduit(): Collection
-    {
-        return $this->fk_produit;
-    }
-
-    public function addFkProduit(PRODUIT $fkProduit): static
-    {
-        if (!$this->fk_produit->contains($fkProduit)) {
-            $this->fk_produit->add($fkProduit);
-            $fkProduit->setFkPhotos($this);
-        }
-
-        return $this;
-    }
-
-    public function removeFkProduit(PRODUIT $fkProduit): static
-    {
-        if ($this->fk_produit->removeElement($fkProduit)) {
-            // set the owning side to null (unless already changed)
-            if ($fkProduit->getFkPhotos() === $this) {
-                $fkProduit->setFkPhotos(null);
-            }
-        }
 
         return $this;
     }
