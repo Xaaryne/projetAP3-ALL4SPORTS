f1eac8e5b63d77ed49bc46cb787e71fb4a222bf3
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-10-10 16:18:35 +0200

    fix repository

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\PRODUIT;
+use App\Repository\PRODUITRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(PRODUIT $produitRepositary): Response
+    public function index(PRODUITRepository $produitRepositary): Response
     {
         $produit = $produitRepositary->find($_GET['id']);
         return $this->render('article/index.html.twig', [
