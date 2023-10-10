220f30ccf495ede0b9e2789d3ffab31d0c369677
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-10-10 16:08:48 +0200

    ajout de find ["i"]

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,8 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(PRODUIT $produit): Response
+    public function index(PRODUIT $produitRepositary): Response
     {
+        $produit = $produitRepositary->find($_GET['id']);
         return $this->render('article/index.html.twig', [
             "article" => $produit,
             'controller_name' => 'ArticleController',
