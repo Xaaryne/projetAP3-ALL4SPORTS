0b057948777caa6541b128ad7f87fb84c3990ab4
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-11-07 15:27:47 +0100

    ajout en dynamique type sport à accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,18 +1,22 @@
 <?php
-
 namespace App\Controller;
 
+use App\Repository\LISTESPORTRepository;
+use App\Repository\PHOTOSRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(LISTESPORTRepository $listesportrepository,PHOTOSRepository $photosrepository): Response
     {
+        $listesport = $listesportrepository->findAll();
+        $photos = $photosrepository->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'listesport' => $listesport,
+            'photos' => $photos,
         ]);
     }
 }
