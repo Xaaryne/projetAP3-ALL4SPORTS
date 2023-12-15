930f6b7f91ad950052bbc70acd4b656455eddb48
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-12-15 19:26:04 +0100

    Temp ajout liste catégorie

--- src/Controller/CategorieListController.php
+++ src/Controller/CategorieListController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\PRODUITRepository;
 
 class CategorieListController extends AbstractController
 {
     #[Route('/categorieList/{id}', name: 'app_categorieList')]
-    public function index(int $id): Response
+    public function index(int $id,Request $Request,PRODUITRepository $produitrepository): Response
     {
-
+        $produit = $produitrepository->findAll();
+        $CurrentUrl = $Request->getSchemeAndHttpHost() . $Request->getRequestUri();
         return $this->render('categorieList/index.html.twig', [
             'controller_name' => 'CategorieListController',
-            'id' => $id
+            'id' => $id,
+            'produit' => $produit,
+
 
  
         ]);
