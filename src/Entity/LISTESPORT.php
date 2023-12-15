baa0358b840001903263a20cb4791f3017224916
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2023-12-15 20:08:41 +0100

    fix fk produit-listesport

--- src/Entity/LISTESPORT.php
+++ src/Entity/LISTESPORT.php
@@ -21,8 +21,7 @@ class LISTESPORT
     #[ORM\OneToMany(mappedBy: 'fk_listesport', targetEntity: CLIENTSPORT::class)]
     private Collection $fk_clientsport;
 
-    #[ORM\OneToMany(mappedBy: 'fk_listesport', targetEntity: PRODUIT::class)]
-    private Collection $fk_produit;
+
 
 
 
