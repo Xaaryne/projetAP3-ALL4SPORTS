567b755834b2dc630b8fdba3236160e1dde06147
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-06 00:53:40 +0100

    Suppresion de catégorie en commandes

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -7,15 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CategorieController extends AbstractController
+class CommandesController extends AbstractController
 {
-    #[Route('/categorie', name: 'app_categorie')]
-    public function index(LISTESPORTRepository $listesportrepository): Response
+    #[Route('/Commandes', name: 'app_commandes')]
+    public function index(): Response
     {
-        $listesport = $listesportrepository->findAll();
         return $this->render('categorie/index.html.twig', [
             'controller_name' => 'CategorieController',
-            'listesport' => $listesport,
+
         ]);
     }
 }
