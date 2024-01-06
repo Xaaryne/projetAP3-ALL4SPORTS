8863ec163bb8ccc3aaab3e540fb8e3b40b79f127
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-06 01:31:44 +0100

    Affichage des commandes (non terminé purge nécessaire)

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -6,14 +6,23 @@ use App\Repository\LISTESPORTRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PRODUITRepository;
+use App\Repository\COMMANDESRepository;
+use App\Repository\PANIERRepository;
 
 class CommandesController extends AbstractController
 {
     #[Route('/Commandes', name: 'app_commandes')]
-    public function index(): Response
+    public function index(PRODUITRepository $produitrepository,COMMANDESRepository $commandesrepository, PANIERRepository $panierrepository): Response
     {
+        $produit = $produitrepository->findAll();
+        $commandes = $commandesrepository->findAll();
+        $panier = $panierrepository->findAll();
         return $this->render('Commandes/index.html.twig', [
             'controller_name' => 'CommandesController',
+            'produit' => $produit,
+            'commandes' => $commandes,
+            'panier' => $panier,
 
         ]);
     }
