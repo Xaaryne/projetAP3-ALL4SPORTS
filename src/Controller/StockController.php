b213ab2fecef8dc0cc98c3c70dd71c75fd046213
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-16 11:03:42 +0100

    lien entre admin et stock

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class StockController extends AbstractController
 {
-    #[Route('/stock', name: 'app_stock')]
-    public function index(): Response
+    #[Route('/stock/{id}', name: 'app_stock')]
+    public function index(int $id,Request $Request): Response
     {
+        $CurrentUrl = $Request->getSchemeAndHttpHost() . $Request->getRequestUri();
         return $this->render('stock/index.html.twig', [
             'controller_name' => 'StockController',
+            'id' => $id,
         ]);
     }
 }
