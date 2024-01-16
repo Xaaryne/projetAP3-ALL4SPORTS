f456bac33e50e5dca46254b1e7c610dfcd3ebc96
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-16 10:51:37 +0100

    création de la page stock

---
+++ src/Controller/StockController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StockController extends AbstractController
+{
+    #[Route('/stock', name: 'app_stock')]
+    public function index(): Response
+    {
+        return $this->render('stock/index.html.twig', [
+            'controller_name' => 'StockController',
+        ]);
+    }
+}
