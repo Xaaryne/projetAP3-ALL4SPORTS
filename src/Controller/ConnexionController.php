2f16f6add0c94aa2a7d1fb592159c97a38275a97
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-26 22:00:13 +0100

    ajout de connexion (non fini)

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CLIENTRepository;
 
 class ConnexionController extends AbstractController
 {
@@ -15,4 +16,29 @@ class ConnexionController extends AbstractController
             'controller_name' => 'ConnexionController',
         ]);
     }
+
+    #[Route('/connexionclient', name: 'app_connexionclient')]
+    public function connection(CLIENTRepository $clientrepository): Response
+    {
+
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $clients = $clientrepository->findAll();
+    $connection = false;
+
+    foreach($client as $clients){
+        if ($client.email == $email){
+            if ($client.password == $password){
+                $connection = true;
+            }
+        }
+    }
+
+
+    return $this->render('connexion/connexion.html.twig', [
+        'controller_name' => 'ConnexionController',
+        'connection' => $connection,
+    ]);
 }
+}
+
