ef824fbf8ceded3f99fca8409a90a3054dcd855c
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-01-26 20:21:38 +0100

    ajout de l'option de créer un utilisateur

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\CLIENT;
+use App\Repository\CLIENTRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class InscriptionController extends AbstractController
 {
@@ -18,15 +21,28 @@ class InscriptionController extends AbstractController
     }
 
     #[Route('/createutilisateur', name: 'app_createutilisateur')]
-    public function createutilisateur(Request $request): Response
+    public function createutilisateur(Request $request,EntityManagerInterface $entityManager,CLIENTRepository $clientrepository): Response
     {
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
         $password = $_POST['password'];
         $email = $_POST['email'];
 
-        
-        return $this->render('inscription/index.html.twig', [
+        $newutilisateur = new CLIENT();
+        $newutilisateur -> setNom($nom);
+        $newutilisateur -> setPrenom($nom);
+        $newutilisateur -> setEmail($email);
+        $newutilisateur -> setPassword($password);
+        $newutilisateur -> setRoles(0);
+        $newutilisateur -> setCode("test");
+        $newutilisateur -> setNbenfants(0);
+
+        $entityManager->persist($newutilisateur);
+
+
+        $entityManager->flush();
+
+        return $this->render('inscription/postInscription.html.twig', [
             'controller_name' => 'InscriptionController',
         ]);
     }
